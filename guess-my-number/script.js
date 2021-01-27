44e14b17ecd602b987f1c5d606cf7acc136322e2
kishanslc <kishany.slc@gmail.com>
2021-01-27 00:38:13 -0500

    commit 3

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -8,13 +8,16 @@ document.querySelector('.guess').value = 23;
 let secretNumber = Math.trunc(Math.random()*20)+1;
 let score=20;
 let highscore = 0;
+const displayMessage = function(message){
+    document.querySelector('.message').textContent=message;
+}
 document.querySelector('.check').addEventListener('click',function(){
 const guess = Number(document.querySelector('.guess').value);
 console.log(guess,typeof guess);
 if (!guess){
-    document.querySelector('.message').textContent='No number';
+    displayMessage('no number');
 } else if (guess===secretNumber){
-    document.querySelector('.message').textContent='Correct number';
+    displayMessage('Correct number');
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor='#60b347';
     document.querySelector('.number').style.width='30rem';
@@ -25,14 +28,14 @@ if (!guess){
 }
 else if (guess!==secretNumber){
     if (score >1){
-        document.querySelector('.message').textContent= guess>secretNumber ? 'too high': 'too low';
+        displayMessage(guess>secretNumber ? 'too high': 'too low');
         score--;
         document.querySelector('.score').textContent = score;
     }
 
 }
 else{
-    document.querySelector('.message').textContent ='You lost the game';
+    displayMessage('You lost the game');
     document.querySelector('.score').textContent = score;
 }
 }
@@ -41,7 +44,7 @@ document.querySelector('.again').addEventListener('click', function()
 {
     score = 20;
     secretNumber = Math.trunc(Math.random()*20)+1;
-    document.querySelector('.message').textContent ='Start guessing...';
+    displayMessage('Start guessing...');
     document.querySelector('.score').textContent = score;
     document.querySelector('.number').textContent='?';
     document.querySelector('.guess').value ='';
