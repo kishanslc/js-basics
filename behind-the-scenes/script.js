2f92fa12b84753b81dad75b0a1fddfac0982366a
kishanslc <kishany.slc@gmail.com>
2021-02-04 03:57:48 -0500

    6

--- behind-the-scenes/script.js
+++ behind-the-scenes/script.js
@@ -2,7 +2,22 @@
 
 function calcAge(birthYear){
     const age = 2037-birthYear;
-    console.log(firstName);
+    
+
+    function printAge()
+    {
+        const output = ' ${firstName},You are ${age},born in ${birthYear}';
+        console.log(output);
+    
+        console.log(output);
+        if (birthYear >= 1981 && birthYear <= 1996){
+            var millenial = true;
+            const str = 'oh, and you are a millenial, ${firstName}';
+            console.log(str);
+        }
+        console.log(millenial);
+    }
+    printAge();
     return age;
 }
 const firstName = 'jonas';
